31f96ebf96cb97944c8e603b589588c43fce5980
Orlando Carmona <orcabe86@gmail.com>
2022-10-08 15:30:56 -0600

    Agregar assets.

---
+++ YoutubeClone/YoutubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeClone
+//
+//  Created by Hector Carmona on 9/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
